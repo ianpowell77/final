5634c96990544766a5ce4a96a1a5aff06512b448
Ian Powell <ianpowell77@yahoo.com>
2017-10-13 09:31:52 -0400

    :wq ` a 	.DS_Store

--- inc/footer.php
+++ inc/footer.php
@@ -1,9 +1,9 @@
 <footer>
 	<div class="footer-container">
 		<ul class="social-media">
-			<li><a href="#"><img src="../lib/img/twitter.svg"></a></li>
-			<li><a href="#"><img src="../lib/img/facebook.svg"></a></li>
-			<li><a href="#"><img src="../lib/img/youtube.svg"></a></li>
+			<li><a href="#"><img src="lib/img/twitter.svg"></a></li>
+			<li><a href="#"><img src="lib/img/facebook.svg"></a></li>
+			<li><a href="#"><img src="lib/img/youtube.svg"></a></li>
 		</ul>
 		<img class="footer-logo" src="lib/img/headphones.svg">
 		<div class="copyright">
